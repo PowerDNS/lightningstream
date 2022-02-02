a7929c508f399adfbdd88c644bd01e2b386303e0
Konrad Wojas <github@m.wojas.nl>
2022-02-02 23:23:04 +0800

    lmdbenv: add IterUpdate strategy with fixes
    Also fixed upstream source.

---
+++ lmdbenv/strategy/iterupdate.go
@@ -0,0 +1,121 @@
+package strategy
+
+import (
+	"bytes"
+
+	"github.com/PowerDNS/lmdb-go/lmdb"
+	"github.com/pkg/errors"
+)
+
+// IterUpdate implements the IterUpdate strategy.
+//
+// Iter over all existing keys and update all URLs we are processing:
+//
+// - Add new keys and data, just like in the `update` strategy.
+// - But also remove codes from urls that have disappeared for this namespace.
+//
+// Once we have reached the end of the database, we use `Update` instead of `Append`.
+//
+// Prerequisites:
+//
+// - Sorted input
+//
+// Uses: merging data
+func IterUpdate(txn *lmdb.Txn, dbi lmdb.DBI, it Iterator) error {
+	c, err := txn.OpenCursor(dbi)
+	if err != nil {
+		return errors.Wrap(err, "open cursor")
+	}
+	defer c.Close()
+
+	err = iterBoth(it, c, func(itKey, dbKey, dbVal []byte, itEOF, dbEOF bool) error {
+		//log.Printf("@@@ args: itkey=%s, dbkey=%s, dbVal=%s, itEOF=%v, dbEOF=%v", string(itKey), string(dbKey), string(dbVal), itEOF, dbEOF)
+
+		if itEOF || itKey == nil {
+			val, err := it.Clean(dbVal)
+			if err != nil {
+				return errors.Wrap(err, "clean")
+			}
+			if val == nil {
+				//log.Printf("@@@ Del itKey nil")
+				err = c.Del(0)
+				if err != nil {
+					return errors.Wrap(err, "del cleaned")
+				}
+				return nil
+			}
+			if bytes.Equal(val, dbVal) {
+				//log.Printf("@@@ Same value")
+				return nil // Already the same, no need to Put
+			}
+			err = txn.Put(dbi, dbKey, val, 0) // Do not touch cursor
+			if err != nil {
+				return errors.Wrap(err, "put cleaned")
+			}
+			return nil
+		}
+
+		// We need the value for all the code below
+		val, err := it.Merge(nil)
+		if err != nil {
+			return errors.Wrap(err, "merge")
+		}
+
+		// At end of database, append any new item
+		if dbEOF {
+			if len(val) == 0 {
+				return nil
+			}
+			// Append all remaining keys
+			err = c.Put(itKey, val, lmdb.Append) // Safe to use cursor, because at end
+			//log.Printf("@@@ Append %s", string(itKey))
+			if err != nil {
+				return errors.Wrap(err, "lmdb append")
+			}
+			return nil
+		}
+
+		// Iterator behind database cursor, add new keys
+		if dbKey == nil {
+			if len(val) == 0 {
+				return nil
+			}
+			// Insert new key with normal Put
+			//log.Printf("@@@ Put %s", string(itKey))
+			err = txn.Put(dbi, itKey, val, 0) // Do not touch cursor
+			if err != nil {
+				return errors.Wrap(err, "put")
+			}
+			return nil
+		}
+
+		val, err = it.Merge(dbVal)
+		if err != nil {
+			return errors.Wrap(err, "merge")
+		}
+		// Both keys the same, overwrite value if different
+		if len(val) == 0 {
+			//log.Printf("@@@ Del")
+			err = c.Del(0)
+			if err != nil {
+				return errors.Wrap(err, "del")
+			}
+			return nil
+		}
+		if bytes.Equal(val, dbVal) {
+			//log.Printf("@@@ Same value")
+			return nil // Already the same, no need to Put
+		}
+		//log.Printf("@@@ Put %v", val)
+		err = txn.Put(dbi, itKey, val, 0) // Do not touch cursor
+		if err != nil {
+			return errors.Wrap(err, "put current")
+		}
+		return nil
+	})
+	if err != nil {
+		return errors.Wrap(err, "iterBoth")
+	}
+
+	return nil
+}
---
+++ lmdbenv/strategy/iterupdate_test.go
@@ -0,0 +1,308 @@
+package strategy
+
+import (
+	"testing"
+
+	"powerdns.com/platform/lightningstream/lmdbenv"
+)
+
+func TestIterUpdate_empty(t *testing.T) {
+	// Only replace exiting items
+	oldItems := []lmdbenv.KVString{}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"yyy", ""},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "X1"},
+		{"zzz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_merge(t *testing.T) {
+	// Only replace exiting items
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "A1Xa"},
+		{"zzz", "A4Xz"},
+	}
+	doStrategyTestNS(t, IterUpdate, oldItems, items, exp, 'A')
+}
+
+func TestIterUpdate_merge_same(t *testing.T) {
+	// Only replace exiting items
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "a"},
+		{"zzz", "z"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_merge_with_before(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aa", "XA"},
+		{"aaa", "XA"},
+		{"zzz", "XZ"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "X1"},
+		{"zzz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_merge_with_after(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+		{"zzzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "X1"},
+		{"zzz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_merge_with_prepend(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aa", "a"},
+		{"aaa", "1"},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aa", "Xa"},
+		{"aaa", "X1"},
+		{"zzz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_merge_with_append(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"zzz", "4"},
+		{"zzzz", "z"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "X1"},
+		{"zzz", "X4"},
+		{"zzzz", "Xz"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_insert(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"ccc", "2"},
+		{"ddd", "3"},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "X1"},
+		{"ccc", "X2"},
+		{"ddd", "X3"},
+		{"zzz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_del(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"ccc", ""},
+		{"ddd", ""},
+		{"zzz", ""},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, nil)
+}
+
+func TestIterUpdate_del_insert_del(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", ""},
+		{"bbb", "b"},
+		{"zzz", ""},
+	}
+	exp := []lmdbenv.KVString{
+		{"bbb", "Xb"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_del_put_del(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"ccc", "Xc"},
+		{"ddd", "Xd"},
+		{"eee", "Xe"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", ""},
+		{"ddd", "3"},
+		{"zzz", ""},
+	}
+	exp := []lmdbenv.KVString{
+		{"ddd", "X3"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_insert_del_insert(t *testing.T) {
+	oldItems := []lmdbenv.KVString{
+		{"ddd", "Xd"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"ddd", ""},
+		{"eee", "3"},
+		{"zzz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaa", "X1"},
+		{"eee", "X3"},
+		{"zzz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_wider(t *testing.T) {
+	// Adding items before and after existing set
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"c", "Xc"},
+		{"d", "Xd"},
+		{"f", "Xf"},
+		{"g", "Xg"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"a", "1"},
+		{"aaa", "1"},
+		{"bbbb", "2"},
+		{"c", "3"},
+		{"d", "3"},
+		{"dddddd", "4"},
+		{"zzz", "4"},
+		{"zzzzzzzzz", "5"},
+	}
+	exp := []lmdbenv.KVString{
+		{"a", "X1"},
+		{"aaa", "X1"},
+		{"bbbb", "X2"},
+		{"c", "X3"},
+		{"d", "X3"},
+		{"dddddd", "X4"},
+		{"zzz", "X4"},
+		{"zzzzzzzzz", "X5"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdate_narrower(t *testing.T) {
+	// Only adding items between existing set
+	oldItems := []lmdbenv.KVString{
+		{"aaa", "Xa"},
+		{"c", "Xc"},
+		{"d", "Xd"},
+		{"f", "Xf"},
+		{"g", "Xg"},
+		{"zzz", "Xz"},
+	}
+	items := []lmdbenv.KVString{
+		{"aaaa", "1"},
+		{"bbbb", "2"},
+		{"c", "3"},
+		{"d", "3"},
+		{"dddddd", "4"},
+		{"zz", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aaaa", "X1"},
+		{"bbbb", "X2"},
+		{"c", "X3"},
+		{"d", "X3"},
+		{"dddddd", "X4"},
+		{"zz", "X4"},
+	}
+	doStrategyTest(t, IterUpdate, oldItems, items, exp)
+}
+
+func TestIterUpdateNS(t *testing.T) {
+	items1 := []lmdbenv.KVString{
+		{"aa", "Z1"}, // will not be touched
+		{"aaa", "A1Z4"},
+		{"aaa3", "A1Z4"}, // will only have A stripped
+		{"bbbb", "C2"},
+		{"c", "X3"},
+		{"cc", "Z3"}, // will not be touched
+		{"dddddd", "X4"},
+		{"del", "A9"},  // will be deleted
+		{"zz", "V1Z4"}, // will not be touched
+	}
+	items2 := []lmdbenv.KVString{
+		{"aaa", "1"},
+		{"aaa2", "2"}, // will be added
+		{"aaa3", ""},
+		{"bbbb", "2"},
+		{"c", "3"},
+		{"dddddd", "4"},
+	}
+	exp := []lmdbenv.KVString{
+		{"aa", "Z1"},
+		{"aaa", "A1Z4"},
+		{"aaa2", "A2"},
+		{"aaa3", "Z4"},
+		{"bbbb", "A2C2"},
+		{"c", "A3X3"},
+		{"cc", "Z3"},
+		{"dddddd", "A4X4"},
+		{"zz", "V1Z4"},
+	}
+	doStrategyTestNS(t, IterUpdate, items1, items2, exp, 'A')
+}
