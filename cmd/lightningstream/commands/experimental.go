2424d2a3a7404214ac5d19fc7f98b7f41aa14972
Konrad Wojas <github@m.wojas.nl>
2023-03-03 22:12:19 +0800

    Experimental migrate-timestamps command

---
+++ cmd/lightningstream/commands/experimental.go
@@ -0,0 +1,230 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/PowerDNS/lmdb-go/lmdb"
+	"github.com/PowerDNS/lmdb-go/lmdbscan"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"powerdns.com/platform/lightningstream/lmdbenv"
+	"powerdns.com/platform/lightningstream/lmdbenv/header"
+	"powerdns.com/platform/lightningstream/utils"
+)
+
+func init() {
+	rootCmd.AddCommand(experimentalCmd)
+
+	experimentalCmd.AddCommand(migrateTimestampsCmd)
+	migrateTimestampsCmd.Flags().StringP("database", "d", "",
+		"Named database to operate on")
+	migrateTimestampsCmd.Flags().String("src-dbi", "", "Source DBI")
+	migrateTimestampsCmd.Flags().String("dst-dbi", "", "Destination DBI")
+	migrateTimestampsCmd.Flags().Bool("add-delete-entries", false,
+		"Create 24-byte deletion headers for entries marked as deleted in the "+
+			"source database.")
+	migrateTimestampsCmd.Flags().Bool("ignore-src-dbi-not-present", false,
+		"Do not exit with an error if the source DBI does not exist, ignore it.")
+	_ = migrateTimestampsCmd.MarkFlagRequired("database")
+	_ = migrateTimestampsCmd.MarkFlagRequired("src-dbi")
+	_ = migrateTimestampsCmd.MarkFlagRequired("dst-dbi")
+}
+
+var experimentalCmd = &cobra.Command{
+	Use:   "experimental",
+	Short: "Experimental commands, may disappear or change in any future version",
+	Run: func(cmd *cobra.Command, args []string) {
+		_ = cmd.Help()
+	},
+}
+
+const migrateTimestampsLong = `
+Migrate timestamps from one DBI to another DBI
+
+Migrate timestamps from one DBI to a target DBI by key. The only requirement
+is that the key is the same, and that the first 8 bytes of the value represent
+the timestamp. If the target timestamp is higher than the source, it will not
+be updated.
+
+This command can be used for both native DBIs, and for shadow DBIs.
+
+This command will abort the transaction and exit with an error if any value
+shorter than 8 bytes is encountered.
+
+Example to migrate shard records when moving from PowerDNS Auth 4.7 to 4.8:
+
+    ... migrate-timestamps --add-deleted-entries --database shard --src-dbi _sync_shadow_records --dst-dbi records_v5
+
+`
+
+var migrateTimestampsCmd = &cobra.Command{
+	Use:          "migrate-timestamps",
+	Short:        "Migrate timestamps from one DBI to another DBI",
+	Long:         migrateTimestampsLong,
+	Args:         cobra.NoArgs,
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dbName, err := cmd.Flags().GetString("database")
+		if err != nil {
+			return err
+		}
+		dbConf, exist := conf.LMDBs[dbName]
+		if !exist {
+			return fmt.Errorf("no LMDB with name %q configured", dbName)
+		}
+		srcDBIName, err := cmd.Flags().GetString("src-dbi")
+		if err != nil {
+			return err
+		}
+		dstDBIName, err := cmd.Flags().GetString("dst-dbi")
+		if err != nil {
+			return err
+		}
+		if srcDBIName == dstDBIName {
+			return fmt.Errorf("source and destination DBI cannot be the same")
+		}
+		ignoreNotPresent, err := cmd.Flags().GetBool("ignore-src-dbi-not-present")
+		if err != nil {
+			return err
+		}
+		addDeleted, err := cmd.Flags().GetBool("add-delete-entries")
+		if err != nil {
+			return err
+		}
+
+		env, err := lmdbenv.NewWithOptions(dbConf.Path, dbConf.Options)
+		if err != nil {
+			return err
+		}
+
+		// Actual migration
+		err = env.Update(func(txn *lmdb.Txn) error {
+			txnID := header.TxnID(txn.ID())
+
+			srcDBI, err := txn.OpenDBI(srcDBIName, lmdb.Readonly)
+			if err != nil {
+				if lmdb.IsNotFound(err) && ignoreNotPresent {
+					logrus.Warn("Source DBI not found, no migration performed")
+					return nil
+				}
+				return err
+			}
+			dstDBI, err := txn.OpenDBI(dstDBIName, 0)
+			if err != nil {
+				return err
+			}
+
+			// Update the timestamps in dst from src
+			dstScan := lmdbscan.New(txn, dstDBI)
+			defer dstScan.Close()
+			for dstScan.Scan() {
+				key := dstScan.Key()
+
+				// Destination value
+				dstVal := dstScan.Val()
+				if len(dstVal) < header.MinHeaderSize {
+					return fmt.Errorf("dst: value too short for header for key %s: %w",
+						utils.DisplayASCII(key), err)
+				}
+				dstTS, err := header.ParseTimestamp(dstVal)
+				if err != nil {
+					return fmt.Errorf("dst: value too short for key %s: %w",
+						utils.DisplayASCII(key), err)
+				}
+
+				// Source value
+				srcVal, err := txn.Get(srcDBI, key)
+				if err != nil {
+					if lmdb.IsNotFound(err) {
+						continue
+					}
+					return err
+				}
+				srcTS, err := header.ParseTimestamp(srcVal)
+				if err != nil {
+					return fmt.Errorf("src: value too short for key %s: %w",
+						utils.DisplayASCII(key), err)
+				}
+
+				// Do not touch if newer
+				if dstTS >= srcTS {
+					continue
+				}
+
+				// Update the timestamp and txnID
+				copy(dstVal, srcVal[:8])
+				binary.BigEndian.PutUint64(dstVal[8:16], uint64(txnID))
+				if err := txn.Put(dstDBI, key, dstVal, 0); err != nil {
+					return err
+				}
+			}
+			if err := dstScan.Err(); err != nil {
+				return err
+			}
+
+			if !addDeleted {
+				return nil // skip the rest
+			}
+			// Any key present in src and missing in dst is marked as deleted
+			// with the timestamp in src, if the src entry is marked as deleted.
+			srcScan := lmdbscan.New(txn, srcDBI)
+			defer srcScan.Close()
+			for srcScan.Scan() {
+				key := srcScan.Key()
+
+				// Source value
+				srcVal := srcScan.Val()
+				srcTS, err := header.ParseTimestamp(srcVal)
+				if err != nil {
+					return fmt.Errorf("src: value too short for key %s: %w",
+						utils.DisplayASCII(key), err)
+				}
+				srcIsDeleted := false
+				if len(srcVal) == 8 {
+					srcIsDeleted = true // old style (0.2.0) DBI
+				} else {
+					h, appVal, err := header.Parse(srcVal)
+					if err != nil {
+						return fmt.Errorf("src: invalid header for key %s: %w",
+							utils.DisplayASCII(key), err)
+					}
+					srcIsDeleted = h.Flags.IsDeleted()
+					if srcIsDeleted && len(appVal) > 0 {
+						return fmt.Errorf("src: deleted but non-empty app value: key %s",
+							utils.DisplayASCII(key))
+					}
+				}
+				if !srcIsDeleted {
+					continue
+				}
+
+				// Only continue if the key does not exist in the destination
+				_, err = txn.Get(dstDBI, key)
+				if err != nil && !lmdb.IsNotFound(err) {
+					return err
+				}
+				if err == nil {
+					continue
+				}
+
+				// Create a deletion marker
+				delVal := make([]byte, header.MinHeaderSize)
+				header.PutBasic(delVal, srcTS, txnID, header.FlagDeleted)
+				if err := txn.Put(dstDBI, key, delVal, 0); err != nil {
+					return err
+				}
+			}
+			if err := srcScan.Err(); err != nil {
+				return err
+			}
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
