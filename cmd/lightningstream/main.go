d17d41a745b7e2e095511cfc3ffef37656a8161a
Konrad Wojas <github@m.wojas.nl>
2023-03-14 23:08:00 +0800

    Enabled net/http/pprof endpoints

--- cmd/lightningstream/main.go
+++ cmd/lightningstream/main.go
@@ -7,6 +7,9 @@ import (
 	_ "github.com/PowerDNS/simpleblob/backends/fs"
 	_ "github.com/PowerDNS/simpleblob/backends/memory"
 	_ "github.com/PowerDNS/simpleblob/backends/s3"
+
+	// Expose pprof in the webserver
+	_ "net/http/pprof"
 )
 
 // version is overridden during the build with the go linker
