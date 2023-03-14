e973e1d7434a6aecd9e409135993033a9dfbce72
Konrad Wojas <github@m.wojas.nl>
2023-03-14 22:58:34 +0800

    proto: patch generated code to reduce memory use
    The generated ptotobuf code made copies of all key/value byte slices.
    This patches the generated code to directly return the byte slices.
    
    In local testing, this reduced memory allocations when loading snapshots
    by about 60%.
    
    Next step is to rewrite the protobuf handling to iterate over the
    protobuf data instead of creating the []KV slices, but that requires a
    significant rewrite.

--- snapshot/regenerate.sh
+++ snapshot/regenerate.sh
@@ -8,8 +8,11 @@ export PATH="$GOBIN:$PATH"
 
 set -ex
 
-# Also mentioned in tools.go
-go install github.com/gogo/protobuf/protoc-gen-gogofast
-go install github.com/bufbuild/buf/cmd/buf
+# No longer updated, so latest should be safe
+go install github.com/gogo/protobuf/protoc-gen-gogofast@latest
+# Last one to include the protoc compat command
+go install github.com/bufbuild/buf/cmd/buf@v1.0.0-rc12
 buf protoc --gogofast_out=. snapshot.proto
 
+# FIXME: Currently need this to prevent copying all byte slices
+patch -p1 < patch.diff
