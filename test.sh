90d96beec54e60c4222430a9fc91c5c89715221e
Luit van Drongelen <luit.van-drongelen@open-xchange.com>
2026-04-29 11:32:47 +0200

    add a check for update-docs.sh
    removed the localhost minio setup as it's no longer used

--- test.sh
+++ test.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-if [ ! -z "$SIMPLEBLOB_TEST_S3_CONFIG" ]; then
-    echo "* Using existing SIMPLEBLOB_TEST_S3_CONFIG=$SIMPLEBLOB_TEST_S3_CONFIG"
-elif curl -v --connect-timeout 2 http://localhost:4730/ 2>&1 | grep --silent MinIO ; then
-    echo "* Using MinIO on localhost for tests"
-    export SIMPLEBLOB_TEST_S3_CONFIG="$PWD/docker/test-minio.json"
-else
-    echo "* MinIO not running on localhost, skipping S3 tests"
-fi
-
 set -ex
 
 go test -count=1 "$@" ./...
@@ -24,4 +15,3 @@ go test -count 20 -run TestSyncer_Sync_startup ./syncer
 # Configure linters in .golangci.yml
 GOBIN="$PWD/bin" go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.0.2
 ./bin/golangci-lint run
-
